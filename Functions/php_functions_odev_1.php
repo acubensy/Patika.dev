c3173e135c2a7272feddee4e43713f2c6a8ac45a
acubensy <79402056+acubensy@users.noreply.github.com>
2023-08-19 17:36:10 +0300

    Add files via upload

---
+++ Functions/php_functions_odev_1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+function ucgen($sayi){
+
+ for($i=1; $i<=$sayi; $i++){
+ 
+  echo "</br> ";
+    for($k=1; $k<$i+1; $k++)
+    	
+        
+ 	    echo "0" . " ";
+		
+
+ }
+
+
+}
+
+ucgen(15);
+?>
+
+</body>
+</html>
